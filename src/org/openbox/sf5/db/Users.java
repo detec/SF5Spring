e8c925eec623527b96269f8ebc68ee45c21b5b92
detec <dlit@yandex.ru>
2015-08-21 17:12:28 +0300

    Corrected few mistakes

--- src/org/openbox/sf5/db/Users.java
+++ src/org/openbox/sf5/db/Users.java
@@ -28,63 +28,72 @@ public class Users implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
-	@Column(name="username", unique = false, nullable = false, length = 50)
-		private String username;
+	private long id;
 
-		public void setusername(String username) {
-			this.username = username;
-		}
+	public long getId() {
 
-		public String getusername() {
-			return username;
-		}
+		return this.id;
+	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
 
+	@Column(name = "username", unique = false, nullable = false, length = 50)
+	private String username;
+
+	public void setusername(String username) {
+		this.username = username;
+	}
+
+	public String getusername() {
+		return username;
+	}
 
 	@Override
 	public String toString() {
 		return username;
 	}
 
-	@Column(name="Password", unique = false, nullable = false, length = 15)
+	@Column(name = "Password", unique = false, nullable = false, length = 15)
 	private String Password;
 
 	public String getPassword() {
 		return Password;
 	}
 
- 	public void setPassword(String Password) {
+	public void setPassword(String Password) {
 		this.Password = Password;
 	}
 
-	@Column(name="enabled", unique = false, nullable = false)
+	@Column(name = "enabled", unique = false, nullable = false)
 	private boolean enabled;
 
 	public boolean getenabled() {
 		return enabled;
 	}
 
- 	public void setenabled(boolean enabled) {
+	public void setenabled(boolean enabled) {
 		this.enabled = enabled;
 	}
 
-
 	@OneToMany(mappedBy = "parent_id", fetch = FetchType.EAGER, orphanRemoval = true)
-	@Cascade({CascadeType.SAVE_UPDATE, CascadeType.DELETE})
-	@OrderColumn(name="LineNumber")
+	@Cascade({ CascadeType.SAVE_UPDATE, CascadeType.DELETE })
+	@OrderColumn(name = "LineNumber")
 	public List<Usersauthorities> authorities;
 
 	public List<Usersauthorities> getauthorities() {
 		return authorities;
 	}
 
- 	public void setauthorities(List<Usersauthorities> authorities) {
+	public void setauthorities(List<Usersauthorities> authorities) {
 		this.authorities = authorities;
 	}
 
-	public Users(String username, String Password, boolean enabled, List<Usersauthorities> authorities) {
+	public Users(String username, String Password, boolean enabled,
+			List<Usersauthorities> authorities) {
 
-	this.username= username;
+		this.username = username;
 		this.Password = Password;
 		this.enabled = enabled;
 		this.authorities = authorities;
@@ -96,29 +105,27 @@ public class Users implements Serializable {
 
 	@Override
 	public boolean equals(Object other) {
-    if (other == null) {
-		return false;
-	}
-    if (other == this) {
-		return true;
-	}
+		if (other == null) {
+			return false;
+		}
+		if (other == this) {
+			return true;
+		}
 
-    if (!(other instanceof Users)) {
-		return false;
-	}
-    Users otherUsers = (Users) other;
+		if (!(other instanceof Users)) {
+			return false;
+		}
+		Users otherUsers = (Users) other;
 		if (otherUsers.username.equals(username)
-	&& otherUsers.Password.equals(Password)
-	&& otherUsers.enabled == enabled
-	&& otherUsers.authorities.equals(authorities)
-) {
+				&& otherUsers.Password.equals(Password)
+				&& otherUsers.enabled == enabled
+				&& otherUsers.authorities.equals(authorities)) {
 			return true;
 		} else {
 			return false;
 		}
 
-}
-
+	}
 
 	public void initialize() {
 
@@ -127,7 +134,6 @@ public class Users implements Serializable {
 				.ObjectsCriterionList(Users.class, criterea);
 		if (adminsList.isEmpty()) {
 			List<Usersauthorities> list = new ArrayList<>();
-
 
 			Users admin = new Users("admin", "1", true, list);
 			list.add(new Usersauthorities("admin", "ROLE_ADMIN", admin));
