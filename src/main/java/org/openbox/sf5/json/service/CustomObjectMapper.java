6b2d5934669213948f9d2099348a6b434c249071
detec <dlit@yandex.ru>
2015-11-27 06:48:57 +0200

    Added pretty printing

--- src/main/java/org/openbox/sf5/json/service/CustomObjectMapper.java
+++ src/main/java/org/openbox/sf5/json/service/CustomObjectMapper.java
@@ -1,5 +1,31 @@
 package org.openbox.sf5.json.service;
 
-public class CustomObjectMapper {
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class CustomObjectMapper extends ObjectMapper {
+
+	private static final long serialVersionUID = -3672344428949816140L;
+
+	public CustomObjectMapper(){
+		// SerializationFeature for changing how JSON is written
+
+		// to enable standard indentation ("pretty-printing"):
+		this.enable(SerializationFeature.INDENT_OUTPUT);
+		// to allow serialization of "empty" POJOs (no properties to serialize)
+		// (without this setting, an exception is thrown in those cases)
+		this.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		// to write java.util.Date, Calendar as number (timestamp):
+		this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		// DeserializationFeature for changing how JSON is read as POJOs:
+
+		// to prevent exception when encountering unknown property:
+		this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		// to allow coercion of JSON empty String ("") to null Object value:
+		this.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+	}
+
 
 }
