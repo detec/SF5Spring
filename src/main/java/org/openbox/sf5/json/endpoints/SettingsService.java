40acff5ebfc1904fce3ceed8e6e37ad222acd5ff
detec <dlit@yandex.ru>
2015-12-04 07:29:02 +0200

    Started to add digest authentication

--- src/main/java/org/openbox/sf5/json/endpoints/SettingsService.java
+++ src/main/java/org/openbox/sf5/json/endpoints/SettingsService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @EnableWebMvc
 @RestController
+@PreAuthorize("hasRole('ROLE_USER')")
 @RequestMapping(value = "/json/usersettings/", headers = "Accept=*/*", produces = "application/json", consumes = "application/json")
 public class SettingsService {
 
