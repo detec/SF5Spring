fd87cbb59180d7988c9acf3fe13c88122e55b0ea
duplyk.a <duplyk.a@developer.malbi.lan>
2015-12-30 15:11:21 +0200

    fixed deserialization issue.

--- src/main/java/org/openbox/sf5/json/service/CustomObjectMapper.java
+++ src/main/java/org/openbox/sf5/json/service/CustomObjectMapper.java
@@ -8,24 +8,30 @@ public class CustomObjectMapper extends ObjectMapper {
 
 	private static final long serialVersionUID = -3672344428949816140L;
 
-	public CustomObjectMapper(){
+	public CustomObjectMapper() {
 		// SerializationFeature for changing how JSON is written
 
 		// to enable standard indentation ("pretty-printing"):
-		this.enable(SerializationFeature.INDENT_OUTPUT);
+		configure(SerializationFeature.INDENT_OUTPUT, true);
 		// to allow serialization of "empty" POJOs (no properties to serialize)
 		// (without this setting, an exception is thrown in those cases)
-		this.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 		// to write java.util.Date, Calendar as number (timestamp):
-		this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
 		// DeserializationFeature for changing how JSON is read as POJOs:
 
 		// to prevent exception when encountering unknown property:
-		this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		// to allow coercion of JSON empty String ("") to null Object value:
-		this.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-	}
+		configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
 
+		// http://stackoverflow.com/questions/14319573/jersey-can-not-deserialize-instance-of-arraylist-out-of-string
+		// To prevent java.lang.IllegalArgumentException: Can not deserialize
+		// instance of java.util.ArrayList out of START_OBJECT token
+		// at [Source: N/A; line: -1, column: -1] (through reference chain:
+		// java.util.ArrayList[0]->org.openbox.sf5.model.Settings["user"]->org.openbox.sf5.model.Users["authorities"])
+		configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+	}
 
 }
