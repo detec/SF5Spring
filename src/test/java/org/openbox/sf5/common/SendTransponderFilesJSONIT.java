b07f914a5727ca29b66080a80c08c0d2e09adce4
duplyk.a <dlit@yandex.ru>
2016-08-15 16:15:32 +0300

    Committing erroneousely unadded class.

--- src/test/java/org/openbox/sf5/common/SendTransponderFilesJSONIT.java
+++ src/test/java/org/openbox/sf5/common/SendTransponderFilesJSONIT.java
@@ -1,26 +1,31 @@
 package org.openbox.sf5.common;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
-import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 import org.junit.Before;
 import org.junit.Test;
 import org.openbox.sf5.json.endpoints.AbstractServiceTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 public class SendTransponderFilesJSONIT extends AbstractServiceTest {
 
-	private static final String servicePath = "transponders";
+	private static final String servicePath = "transponders/";
 
 	@Before
 	public void setUp() {
@@ -34,16 +39,54 @@ public class SendTransponderFilesJSONIT extends AbstractServiceTest {
 
 		transponderFilesPathes.forEach(t -> {
 
-			FileDataBodyPart filePart = new FileDataBodyPart("file", t.toFile());
+			// FileDataBodyPart filePart = new FileDataBodyPart("file",
+			// t.toFile());
+			//
+			// @SuppressWarnings("resource")
+			// final FormDataMultiPart multipart = (FormDataMultiPart) new
+			// FormDataMultiPart().field("foo", "bar")
+			// .bodyPart(filePart);
+			//
+			// Invocation.Builder invocationBuilder =
+			// serviceTarget.request(MediaType.APPLICATION_JSON);
+			//
+			// Response responsePost = invocationBuilder.post(Entity.entity(
+			//
+			// multipart, multipart.getMediaType()));
 
-			@SuppressWarnings("resource")
-			final FormDataMultiPart multipart = (FormDataMultiPart) new FormDataMultiPart().field("foo", "bar")
-					.bodyPart(filePart);
+			RestTemplate restTemplate = new RestTemplate();
 
-			Invocation.Builder invocationBuilder = serviceTarget.path("upload").request(MediaType.APPLICATION_JSON);
+			LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
 
-			Response responsePost = invocationBuilder.post(Entity.entity(multipart, multipart.getMediaType()));
-			Boolean result = responsePost.readEntity(Boolean.class);
+			map.add("file", new FileSystemResource(t.toFile()));
+
+			HttpHeaders headers = new HttpHeaders();
+
+			headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+			List<MediaType> acceptTypes = new ArrayList<>();
+			acceptTypes.add(MediaType.APPLICATION_JSON);
+			headers.setAccept(acceptTypes);
+
+			HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
+			ResponseEntity<String> stringResponse = restTemplate.exchange(serviceTarget.getUri(), HttpMethod.POST,
+					requestEntity, String.class);
+
+			HttpStatus statusCode = stringResponse.getStatusCode();
+
+			assertEquals(statusCode, HttpStatus.OK);
+
+			// Boolean result = responsePost.readEntity(Boolean.class);
+
+			String body = stringResponse.getBody();
+
+			Boolean result = false;
+			try {
+				result = mapper.readValue(body, Boolean.class);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 			assertThat(result.booleanValue()).isTrue();
 
 		});
