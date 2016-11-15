5a91de6253cdef135d360b432daade593115b1ed
duplyk.a <dlit@yandex.ru>
2016-11-15 11:10:15 +0200

    Refactoring.

--- src/main/java/org/openbox/sf5/model/Users.java
+++ src/main/java/org/openbox/sf5/model/Users.java
@@ -2,6 +2,7 @@ package org.openbox.sf5.model;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -38,6 +39,38 @@ public class Users extends AbstractDbEntity implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
+	@Column(name = "username", unique = false, nullable = false, length = 50)
+	@XmlID
+	private String username;
+
+	@Column(name = "Password", unique = false, nullable = false, length = 15)
+	@JsonProperty("password")
+	private String Password;
+
+	@Column(name = "enabled", unique = false, nullable = false)
+	private boolean enabled;
+
+	@OneToMany(mappedBy = "parent_id", fetch = FetchType.EAGER, orphanRemoval = true)
+	@Cascade({ CascadeType.ALL })
+	@OrderColumn(name = "LineNumber")
+	@JsonManagedReference
+	@Valid
+	@NotNull
+	@Size(min = 1)
+	private List<Usersauthorities> authorities;
+
+	public Users(String username, String Password, boolean enabled, List<Usersauthorities> authorities) {
+
+		this.username = username;
+		this.Password = Password;
+		this.enabled = enabled;
+		this.authorities = authorities;
+
+	}
+
+	public Users() {
+	}
+
 	public long getId() {
 
 		return id;
@@ -49,12 +82,7 @@ public class Users extends AbstractDbEntity implements Serializable {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((Password == null) ? 0 : Password.hashCode());
-		result = prime * result + (enabled ? 1231 : 1237);
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -69,29 +97,8 @@ public class Users extends AbstractDbEntity implements Serializable {
 			return false;
 		}
 		Users other = (Users) obj;
-		if (Password == null) {
-			if (other.Password != null) {
-				return false;
-			}
-		} else if (!Password.equals(other.Password)) {
-			return false;
-		}
-		if (enabled != other.enabled) {
-			return false;
-		}
-		if (username == null) {
-			if (other.username != null) {
-				return false;
-			}
-		} else if (!username.equals(other.username)) {
-			return false;
-		}
-		return true;
+		return Objects.equals(id, other.id);
 	}
-
-	@Column(name = "username", unique = false, nullable = false, length = 50)
-	@XmlID
-	private String username;
 
 	public void setusername(String username) {
 		this.username = username;
@@ -106,10 +113,6 @@ public class Users extends AbstractDbEntity implements Serializable {
 		return username;
 	}
 
-	@Column(name = "Password", unique = false, nullable = false, length = 15)
-	@JsonProperty("password")
-	private String Password;
-
 	public String getPassword() {
 		return Password;
 	}
@@ -117,9 +120,6 @@ public class Users extends AbstractDbEntity implements Serializable {
 	public void setPassword(String Password) {
 		this.Password = Password;
 	}
-
-	@Column(name = "enabled", unique = false, nullable = false)
-	private boolean enabled;
 
 	public boolean getenabled() {
 		return enabled;
@@ -129,34 +129,12 @@ public class Users extends AbstractDbEntity implements Serializable {
 		this.enabled = enabled;
 	}
 
-	@OneToMany(mappedBy = "parent_id", fetch = FetchType.EAGER, orphanRemoval = true)
-	// @Cascade({ CascadeType.SAVE_UPDATE, CascadeType.DELETE })
-	@Cascade({ CascadeType.ALL })
-	@OrderColumn(name = "LineNumber")
-	@JsonManagedReference
-	@Valid
-	@NotNull
-	@Size(min = 1)
-	private List<Usersauthorities> authorities;
-
 	public List<Usersauthorities> getauthorities() {
 		return authorities;
 	}
 
 	public void setauthorities(List<Usersauthorities> authorities) {
 		this.authorities = authorities;
-	}
-
-	public Users(String username, String Password, boolean enabled, List<Usersauthorities> authorities) {
-
-		this.username = username;
-		this.Password = Password;
-		this.enabled = enabled;
-		this.authorities = authorities;
-
-	}
-
-	public Users() {
 	}
 
 }
