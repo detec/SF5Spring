4074774fe7f866acc6b4c8abc8de1fe8d86d5d16
WINMAIN\Andrii <andrii.duplyk@nitka.com>
2017-07-20 09:54:12 +0300

    Upgraded Spring version, addedSpring MVC test.

---
+++ src/test/java/org/openbox/sf5/config/AppTestConfiguration.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package org.openbox.sf5.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Inports test beans
+ *
+ * @author Andrii Duplyk
+ *
+ */
+@Configuration
+@ImportResource({ "file:src/test/resources/context/test-autowired-beans.xml" })
+public class AppTestConfiguration {
+
+}
---
+++ src/test/java/org/openbox/sf5/json/mockmvc/TranspondersTests.java
@@ -0,0 +1,44 @@
+package org.openbox.sf5.json.mockmvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openbox.sf5.config.AppTestConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ContextConfiguration(classes = { AppTestConfiguration.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class TranspondersTests {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Value("${jaxrs.path}")
+    private String jsonPath;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    public void getMockTransponders() throws Exception {
+        this.mockMvc.perform(
+                get(String.join("", "/", jsonPath, "/transponders/")).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(204));
+    }
+}
