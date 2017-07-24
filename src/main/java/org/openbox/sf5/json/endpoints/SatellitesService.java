ecdae60626bd40cf2129386303c36b220a80d1d8
WINMAIN\Andrii <andrii.duplyk@nitka.com>
2017-07-24 09:58:21 +0300

    Refactoring.

--- src/main/java/org/openbox/sf5/json/endpoints/SatellitesService.java
+++ src/main/java/org/openbox/sf5/json/endpoints/SatellitesService.java
@@ -1,6 +1,7 @@
 package org.openbox.sf5.json.endpoints;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.core.MediaType;
 
@@ -18,45 +19,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-// http://websystique.com/springmvc/spring-mvc-4-restful-web-services-crud-example-resttemplate/
-// http://stackoverflow.com/questions/17128463/overriding-requestmapping-on-springmvc-controller
 @RestController
 @EnableWebMvc
 @RequestMapping(value = "${jaxrs.path}/satellites/", produces = { "application/xml", "application/json" })
 public class SatellitesService {
 
+    @Autowired
+    private ObjectsController objectController;
+
+    @Autowired
+    private SatellitesJsonizer jsonizer;
+
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
 	public ResponseEntity<List<Satellites>> getAllSatellites() {
 		List<Satellites> satList = objectController.list(Satellites.class);
-		if (satList.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
-
-		return new ResponseEntity<>(satList, HttpStatus.OK);
+        return satList.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+                : new ResponseEntity<>(satList, HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_XML)
 	public ResponseEntity<GenericXMLListWrapper<Satellites>> getAllSatellitesXML()
 			throws NoSuchFieldException, SecurityException, NoSuchMethodException {
+
 		List<Satellites> satList = objectController.list(Satellites.class);
-		if (satList.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);//
-		}
-
-		// we should replace XmlElementWrapper name
-		// Method method = wrapper.getClass().getMethod("getWrappedList");
-
-		return JsonObjectFiller.returnGenericWrapperResponseBySatList(satList, Satellites.class);
+        return satList.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+                : JsonObjectFiller.returnGenericWrapperResponseBySatList(satList, Satellites.class);
 	}
 
 	@RequestMapping(value = "{satelliteId}", method = RequestMethod.GET)
 	public ResponseEntity<Satellites> getSatelliteById(@PathVariable("satelliteId") long satId) {
-		Satellites sat = objectController.select(Satellites.class, satId);
-		if (sat == null) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
 
-		return new ResponseEntity<>(sat, HttpStatus.OK);
+        return Optional.ofNullable(objectController.select(Satellites.class, satId))
+                .map(sat -> new ResponseEntity<>(sat, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
 	}
 
 	@RequestMapping(value = "filter/{type}/{typeValue}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
@@ -64,33 +59,19 @@ public class SatellitesService {
 			@PathVariable("typeValue") String typeValue) {
 
 		List<Satellites> satList = jsonizer.getSatellitesByArbitraryFilter(fieldName, typeValue);
-		if (satList.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
-
-		return new ResponseEntity<>(satList, HttpStatus.OK);
+        return satList.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+                : new ResponseEntity<>(satList, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "filter/{type}/{typeValue}", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML)
 	public ResponseEntity<GenericXMLListWrapper<Satellites>> getSatellitesByArbitraryFilterXML(
 			@PathVariable("type") String fieldName, @PathVariable("typeValue") String typeValue) {
+
 		List<Satellites> satList = jsonizer.getSatellitesByArbitraryFilter(fieldName, typeValue);
-
-		if (satList.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);//
-		}
-
-		return JsonObjectFiller.returnGenericWrapperResponseBySatList(satList, Satellites.class);
-
+        return satList.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+                : JsonObjectFiller.returnGenericWrapperResponseBySatList(satList, Satellites.class);
 	}
 
-	@Autowired
-	private ObjectsController objectController;
-
-	// http://www.mkyong.com/spring3/spring-value-default-value/
-
-	@Autowired
-	private SatellitesJsonizer jsonizer;
 
 	public ObjectsController getObjectController() {
 		return objectController;
